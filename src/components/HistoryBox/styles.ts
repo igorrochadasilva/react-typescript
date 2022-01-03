9a412fae2ce0c6fcf1ea6c05a0d2ad3ff0600f70
igorsilva <igor.silva@performait.com>
2022-01-03 19:26:09 -0300

    seção de animção de elementos do dashboard

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,12 +1,25 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-
+const animate = keyframes`
+    0%{
+        transform: translateX(-100px);    
+        opacity: 0;
+    }
+    50%{        
+        opacity: .3;
+    }
+    100%{
+        transform: translateX(0px);    
+        opacity: 1;
+    }
+`
 interface ILegendProps {
     color: string
 }
 
 export const Container = styled.div`
     width: 100%;
+    
     height: 400px;  
     display: flex;
     flex-direction: column;
@@ -18,6 +31,8 @@ export const Container = styled.div`
     padding: 30px 20px;
     
     border-radius: 7px;
+    
+    animation: ${animate} .5s;
 `
 
 export const ChartContainer = styled.div`
